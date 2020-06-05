60ca04d1d06ec22c24d8e8b69d24d25e8581b174
Marcelo <santosmarcelo1995@live.com>
2020-06-04 23:39:04 -0300

    Next Level Week - Day 3

--- server/src/Controllers/PointsController.ts
+++ server/src/Controllers/PointsController.ts
@@ -67,6 +67,7 @@ class PointsController {
             city, 
             uf
         };
+
         const insertedIds = await trx('points').insert(point);
         
         const point_id = insertedIds[0];
